a2aec115d2fadc76f5ee934088be33014afdcf3d
jokoson <deine.email@example.com>
2024-09-01 11:00:42 +0200

    refactor: Add Col component and update CmsContent attributes
    The code changes in this commit involve adding the Col component and updating the attributes of the CmsContent component in the content.json and components.d.ts files. This allows for the repeatable use of the Col component within the CmsContent component, providing more flexibility in the layout of the content.

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,125 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface NavNav extends Schema.Component {
-  collectionName: 'components_nav_navs';
-  info: {
-    displayName: 'Nav';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String;
-    link: Attribute.String;
-    page: Attribute.Relation<'nav.nav', 'oneToOne', 'api::page.page'>;
-    submenu: Attribute.Component<'items.nav-items', true>;
-    hidden: Attribute.Boolean & Attribute.Required & Attribute.DefaultTo<false>;
-  };
-}
-
-export interface ItemsPostCategoryList extends Schema.Component {
-  collectionName: 'components_items_post_category_lists';
-  info: {
-    displayName: 'PostCategoryList';
-    description: '';
-  };
-  attributes: {
-    categoryList: Attribute.Enumeration<
-      [
-        'KI',
-        'React',
-        'WordPress',
-        'NextJs',
-        'Figma',
-        'WebDesign',
-        'Frontend',
-        'IT',
-        'Algemein',
-        'othrt'
-      ]
-    >;
-  };
-}
-
-export interface ItemsNavItems extends Schema.Component {
-  collectionName: 'components_items_nav_items';
-  info: {
-    displayName: 'NavItems';
-  };
-  attributes: {
-    title: Attribute.String;
-    link: Attribute.String;
-    page: Attribute.Relation<'items.nav-items', 'oneToOne', 'api::page.page'>;
-  };
-}
-
-export interface ItemsListItems extends Schema.Component {
-  collectionName: 'components_cms_list_items';
-  info: {
-    displayName: 'ListItems';
-    description: '';
-  };
-  attributes: {
-    content: Attribute.Text;
-  };
-}
-
-export interface ItemsLevelItems extends Schema.Component {
-  collectionName: 'components_items_level_items';
-  info: {
-    displayName: 'LevelItems';
-  };
-  attributes: {
-    level: Attribute.Enumeration<['Low-Level', 'Middle-Level', 'High-Level']>;
-  };
-}
-
-export interface ItemsImageItems extends Schema.Component {
-  collectionName: 'components_items_image_items';
-  info: {
-    displayName: 'ImageItems';
-  };
-  attributes: {
-    image: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
-  };
-}
-
-export interface ItemsCarouselItems extends Schema.Component {
-  collectionName: 'components_items_carousel_items';
-  info: {
-    displayName: 'CarouselItems';
-    description: '';
-  };
-  attributes: {
-    img: Attribute.Media<'images'>;
-    headline: Attribute.String;
-    content: Attribute.String;
-  };
-}
-
-export interface ItemsAccordionItems extends Schema.Component {
-  collectionName: 'components_cms_accordion_items';
-  info: {
-    displayName: 'AccordionItems';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String;
-    content: Attribute.Blocks;
-  };
-}
-
-export interface MetaSeo extends Schema.Component {
-  collectionName: 'components_meta_seos';
-  info: {
-    displayName: 'SEO';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    description: Attribute.Text & Attribute.Required;
-    ogImage: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-  };
-}
-
 export interface CmsVHero extends Schema.Component {
   collectionName: 'components_cms_v_heroes';
   info: {
@@ -372,18 +252,129 @@ export interface CmsAccordion extends Schema.Component {
   };
 }
 
+export interface MetaSeo extends Schema.Component {
+  collectionName: 'components_meta_seos';
+  info: {
+    displayName: 'SEO';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+    ogImage: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+  };
+}
+
+export interface NavNav extends Schema.Component {
+  collectionName: 'components_nav_navs';
+  info: {
+    displayName: 'Nav';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+    page: Attribute.Relation<'nav.nav', 'oneToOne', 'api::page.page'>;
+    submenu: Attribute.Component<'items.nav-items', true>;
+    hidden: Attribute.Boolean & Attribute.Required & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ItemsPostCategoryList extends Schema.Component {
+  collectionName: 'components_items_post_category_lists';
+  info: {
+    displayName: 'PostCategoryList';
+    description: '';
+  };
+  attributes: {
+    categoryList: Attribute.Enumeration<
+      [
+        'KI',
+        'React',
+        'WordPress',
+        'NextJs',
+        'Figma',
+        'WebDesign',
+        'Frontend',
+        'IT',
+        'Algemein',
+        'othrt'
+      ]
+    >;
+  };
+}
+
+export interface ItemsNavItems extends Schema.Component {
+  collectionName: 'components_items_nav_items';
+  info: {
+    displayName: 'NavItems';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+    page: Attribute.Relation<'items.nav-items', 'oneToOne', 'api::page.page'>;
+  };
+}
+
+export interface ItemsListItems extends Schema.Component {
+  collectionName: 'components_cms_list_items';
+  info: {
+    displayName: 'ListItems';
+    description: '';
+  };
+  attributes: {
+    content: Attribute.Text;
+  };
+}
+
+export interface ItemsLevelItems extends Schema.Component {
+  collectionName: 'components_items_level_items';
+  info: {
+    displayName: 'LevelItems';
+  };
+  attributes: {
+    level: Attribute.Enumeration<['Low-Level', 'Middle-Level', 'High-Level']>;
+  };
+}
+
+export interface ItemsImageItems extends Schema.Component {
+  collectionName: 'components_items_image_items';
+  info: {
+    displayName: 'ImageItems';
+  };
+  attributes: {
+    image: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+  };
+}
+
+export interface ItemsCarouselItems extends Schema.Component {
+  collectionName: 'components_items_carousel_items';
+  info: {
+    displayName: 'CarouselItems';
+    description: '';
+  };
+  attributes: {
+    img: Attribute.Media<'images'>;
+    headline: Attribute.String;
+    content: Attribute.String;
+  };
+}
+
+export interface ItemsAccordionItems extends Schema.Component {
+  collectionName: 'components_cms_accordion_items';
+  info: {
+    displayName: 'AccordionItems';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    content: Attribute.Blocks;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'nav.nav': NavNav;
-      'items.post-category-list': ItemsPostCategoryList;
-      'items.nav-items': ItemsNavItems;
-      'items.list-items': ItemsListItems;
-      'items.level-items': ItemsLevelItems;
-      'items.image-items': ItemsImageItems;
-      'items.carousel-items': ItemsCarouselItems;
-      'items.accordion-items': ItemsAccordionItems;
-      'meta.seo': MetaSeo;
       'cms.v-hero': CmsVHero;
       'cms.update': CmsUpdate;
       'cms.rating': CmsRating;
@@ -402,6 +393,15 @@ declare module '@strapi/types' {
       'cms.carousel': CmsCarousel;
       'cms.button': CmsButton;
       'cms.accordion': CmsAccordion;
+      'meta.seo': MetaSeo;
+      'nav.nav': NavNav;
+      'items.post-category-list': ItemsPostCategoryList;
+      'items.nav-items': ItemsNavItems;
+      'items.list-items': ItemsListItems;
+      'items.level-items': ItemsLevelItems;
+      'items.image-items': ItemsImageItems;
+      'items.carousel-items': ItemsCarouselItems;
+      'items.accordion-items': ItemsAccordionItems;
     }
   }
 }
